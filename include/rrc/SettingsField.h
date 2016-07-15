31b3c6cc01f619c0b40554a15676f39296125ef6
TekatoD <ocnechlahim@mail.ru>
2016-07-15 17:04:10 +0300

    New class for settings field

---
+++ include/rrc/SettingsField.h
@@ -0,0 +1,24 @@
+/**
+ *  @autor tekatod
+ *  @date 7/15/16
+ */
+#pragma once
+
+#include <string>
+
+namespace rrc {
+    class SettingsField {
+    private:
+        union float_bool_str {
+            std::string m_string;
+            float mfloat;
+            bool m_bool;
+            float_bool_str(std::string str) {
+                new (&m_string) std::string(str);
+            }
+            ~float_bool_str() {
+                m_string.~string();
+            }
+        };
+    };
+}
