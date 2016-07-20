6e5607cf726c1f8cb9a6b9f9f76a832ce757cc17
TekatoD <ocnechlahim@mail.ru>
2016-07-20 18:10:14 +0300

    Property

--- include/rrc/Property.h
+++ include/rrc/Property.h
@@ -5,6 +5,10 @@
 #pragma once
 
 #include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <type_traits>
 #include "rrc/CopyOnWrite.h"
 #include "mapbox/variant.hpp"
 
@@ -17,18 +21,122 @@ namespace rrc {
         Property(D&& data) {
             mField = std::forward<D>(data);
         }
-
-        template <class T>
+        //TODO:: get is template. Do something!!!
+        template <typename T>
         T& get() const {
-            return mField->get().get();
+            switch(mField->which()) {
+                case 0:
+                    return getCast<T>(mField->get<std::string>());
+                case 1:
+                    return getCast<T>(mField->get<float>());
+                case 2:
+                    return getCast<T>(mField->get<bool>());
+                case 3:
+                    return getCast<T>(mField->get<int>());
+
+            }
+        }
+
+        int getInt() const {
+            switch(mField->which()) {
+                case 0:
+                    return getCast<int>(mField->get<std::string>());
+                case 1:
+                    return getCast<int>(mField->get<float>());
+                case 2:
+                    return getCast<int>(mField->get<bool>());
+                case 3:
+                    return getCast<int>(mField->get<int>());
+
+            }
+        }
+
+        float getFloat() const {
+            switch(mField->which()) {
+                case 0:
+                    return getCast<float>(mField->get<std::string>());
+                case 1:
+                    return getCast<float>(mField->get<float>());
+                case 2:
+                    return getCast<float>(mField->get<bool>());
+                case 3:
+                    return getCast<float>(mField->get<int>());
+
+            }
+        }
+
+        bool getBool() const {
+            switch(mField->which()) {
+                case 0:
+                    return getCast<bool>(mField->get<std::string>());
+                case 1:
+                    return getCast<bool>(mField->get<float>());
+                case 2:
+                    return getCast<bool>(mField->get<bool>());
+                case 3:
+                    return getCast<bool>(mField->get<int>());
+
+            }
+        }
+
+        std::string getString() const {
+            switch(mField->which()) {
+                case 0:
+                    return getCast<std::string>(mField->get<std::string>());
+                case 1:
+                    return getCast<std::string>(mField->get<float>());
+                case 2:
+                    return getCast<std::string>(mField->get<bool>());
+                case 3:
+                    return getCast<std::string>(mField->get<int>());
+
+            }
         }
 
         template <class D>
         void set(D &&data) {
-            mField = std::forward<D>(data);
+            if(std::is_same<D, bool>::value || std::is_same<D, int>::value ||
+                    std::is_same<D, float>::value || std::is_same<D, std::string>::value) {
+                mField->set<D>(std::forward<D>(data));
+            }
+            else {
+                throw stderr("Unsupported type");
+            };
         }
 
     private:
+        template <typename T>
+        T& getCast(T const &s) const {
+            return s;
+        }
+
+//        template<>
+        int getCast<int>(std::string const &s) const {
+            return atoi(s);
+        }
+
+//        template<>
+        float getCast<float>(std::string const &s) const {
+            return std::stof(s);
+        }
+
+//        template<>
+        bool getCast<bool>(std::string const &s) const {
+            bool res;
+            std::istringstream(s) >> std::boolalpha >> res;
+            return res;
+        }
+
+        template<typename, typename D, class = typename std::enable_if<std::is_arithmetic<D>::value>::type>
+        std::string getCast<std::string>(D &const n) const {
+            return std::to_string(n);
+        };
+
+        template <typename T, class = typename std::enable_if<std::is_arithmetic<T>::value>::type,
+                typename D, class = typename std::enable_if<std::is_arithmetic<D>::value>::type>
+        std::string getCast<T>(D &const n) const {
+            return T(n);
+        };
 //        union float_bool_str {
 //            std::string m_string;
 //            float mfloat;
@@ -41,6 +149,5 @@ namespace rrc {
 //            }
 //        };
         CopyOnWrite<SettingsType> mField;
-        SettingsType test;
     };
 }
