b871cf6c4121c3ffd573a90bb83238e6bd11060f
Arseniy Ivin <arssivka@yandex.ru>
2017-02-16 18:48:05 +0300

    Mechanism holder fix

--- src/rrc/core/MechanismsHolder.cpp
+++ src/rrc/core/MechanismsHolder.cpp
@@ -6,15 +6,35 @@
 #include <rrc/core/MechanismsHolder.h>
 
 
-rrc::MechanismsHolder::MechanismsHolder(std::shared_ptr<rrc::AbstractTaskQueueAdapter> queue)
-        : mAdvertisingMechanism(std::move(queue)) {}
+rrc::MechanismsHolder::MechanismsHolder(std::shared_ptr<rrc::AbstractTaskQueueAdapter> queue,
+                                        QueueAdapterFactory<Task> queueAdapterFactory)
+        : mAdvertisingMechanism(queue), mServiceMechanism(queue), mSettingsMechanism(queue, queueAdapterFactory) {}
 
 
 rrc::AdvertisingMechanism& rrc::MechanismsHolder::getAdvertisingMechanism() {
     return mAdvertisingMechanism;
 }
 
+rrc::ServiceMechanism& rrc::MechanismsHolder::getServiceMechanism() {
+    return mServiceMechanism;
+}
+
+
+rrc::SettingsMechanism& rrc::MechanismsHolder::getSettingsMechanism() {
+    return mSettingsMechanism;
+}
+
 
 const rrc::AdvertisingMechanism& rrc::MechanismsHolder::getAdvertisingMechanism() const {
     return mAdvertisingMechanism;
+}
+
+
+const rrc::ServiceMechanism& rrc::MechanismsHolder::getServiceMechanism() const {
+    return mServiceMechanism;
+}
+
+
+const rrc::SettingsMechanism& rrc::MechanismsHolder::getSettingsMechanism() const {
+    return mSettingsMechanism;
 }
