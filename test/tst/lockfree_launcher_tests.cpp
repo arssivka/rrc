c892c660ddc911953acb54dadb119fa814aefdbc
TekatoD <ocnechlahim@mail.ru>
2017-03-15 16:28:21 +0300

    Lockfree launcher tests.

---
+++ test/tst/lockfree_launcher_tests.cpp
@@ -0,0 +1,161 @@
+/**
+ *  @autor tekatod
+ *  @date 3/15/17
+ */
+
+#include <gtest/gtest.h>
+#include <rrc/lockfree_launcher.h>
+
+using namespace rrc;
+
+class lockfree_launcher_fixture: public ::testing::Test {
+public:
+    lockfree_launcher_fixture() : lockfree_launcher1() {
+
+    }
+
+protected:
+    lockfree_launcher lockfree_launcher1;
+    std::vector<int> check_vector;
+};
+
+TEST_F(lockfree_launcher_fixture, user_task) {
+    lockfree_launcher1.enqueue_user_task([this](){
+        check_vector.push_back(1);
+    });
+    EXPECT_FALSE(lockfree_launcher1.is_multithreading());
+    EXPECT_EQ(check_vector, std::vector<int>());
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1}));
+}
+
+TEST_F(lockfree_launcher_fixture, user_task_duration) {
+    using namespace std::chrono_literals;
+    lockfree_launcher1.enqueue_user_task_for(std::chrono::steady_clock::duration(100ms), [this](){
+        check_vector.push_back(1);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_user_task_for(std::chrono::steady_clock::duration(300ms), [this](){
+        check_vector.push_back(2);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_user_task_for(std::chrono::steady_clock::duration(500ms), [this](){
+        check_vector.push_back(3);
+    });
+    EXPECT_EQ(check_vector, std::vector<int>());
+    std::this_thread::sleep_for(200ms);
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+}
+
+TEST_F(lockfree_launcher_fixture, user_task_at) {
+    using namespace std::chrono_literals;
+    lockfree_launcher1.enqueue_user_task_at(std::chrono::steady_clock::now() + 100ms, [this](){
+        check_vector.push_back(1);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_user_task_at(std::chrono::steady_clock::now() + 300ms, [this](){
+        check_vector.push_back(2);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_user_task_at(std::chrono::steady_clock::now() + 500ms, [this](){
+        check_vector.push_back(3);
+    });
+    EXPECT_EQ(check_vector, std::vector<int>());
+    std::this_thread::sleep_for(200ms);
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+}
+
+TEST_F(lockfree_launcher_fixture, sync_task) {
+    lockfree_launcher1.enqueue_sync_task([this](){
+        check_vector.push_back(1);
+    });
+    EXPECT_FALSE(lockfree_launcher1.is_multithreading());
+    EXPECT_EQ(check_vector, std::vector<int>());
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1}));
+}
+
+TEST_F(lockfree_launcher_fixture, sync_task_duration) {
+    using namespace std::chrono_literals;
+    lockfree_launcher1.enqueue_sync_task_for(std::chrono::steady_clock::duration(100ms), [this](){
+        check_vector.push_back(1);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_sync_task_for(std::chrono::steady_clock::duration(300ms), [this](){
+        check_vector.push_back(2);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_sync_task_for(std::chrono::steady_clock::duration(500ms), [this](){
+        check_vector.push_back(3);
+    });
+    EXPECT_EQ(check_vector, std::vector<int>());
+    std::this_thread::sleep_for(200ms);
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+}
+
+TEST_F(lockfree_launcher_fixture, sync_task_at) {
+    using namespace std::chrono_literals;
+    lockfree_launcher1.enqueue_sync_task_at(std::chrono::steady_clock::now() + 100ms, [this](){
+        check_vector.push_back(1);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_sync_task_at(std::chrono::steady_clock::now() + 300ms, [this](){
+        check_vector.push_back(2);
+        std::this_thread::sleep_for(200ms);
+    });
+    lockfree_launcher1.enqueue_sync_task_at(std::chrono::steady_clock::now() + 500ms, [this](){
+        check_vector.push_back(3);
+    });
+    EXPECT_EQ(check_vector, std::vector<int>());
+    std::this_thread::sleep_for(200ms);
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 2, 3}));
+}
+
+TEST_F(lockfree_launcher_fixture, finalize_task) {
+    lockfree_launcher1.enqueue_finalize_task([this](){
+        check_vector.push_back(1);
+    });
+    EXPECT_FALSE(lockfree_launcher1.is_multithreading());
+    EXPECT_EQ(check_vector, std::vector<int>());
+    lockfree_launcher1.finalize();
+    EXPECT_EQ(check_vector, std::vector<int>({1}));
+    lockfree_launcher1.finalize();
+    EXPECT_EQ(check_vector, std::vector<int>({1}));
+}
+
+TEST_F(lockfree_launcher_fixture, run_stop_task) {
+    lockfree_launcher1.enqueue_user_task([this](){
+        check_vector.push_back(1);
+    });
+    lockfree_launcher1.enqueue_user_task([this](){
+        check_vector.push_back(1);
+    });
+    lockfree_launcher1.enqueue_user_task([this](){
+        lockfree_launcher1.stop();
+    });
+    lockfree_launcher1.enqueue_finalize_task([this](){
+        check_vector.push_back(2);
+    });
+    EXPECT_FALSE(lockfree_launcher1.is_multithreading());
+    EXPECT_EQ(check_vector, std::vector<int>());
+    lockfree_launcher1.run();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 1, 2}));
+    lockfree_launcher1.step();
+    EXPECT_EQ(check_vector, std::vector<int>({1, 1, 2}));
+}
