2c8817f01b06ebaa988ff1741ff53bc48d880ea0
Arseniy Ivin <arssivka@yandex.ru>
2017-03-16 06:12:04 +0300

    Add async worker class

---
+++ include/rrc/async_worker.h
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2016 Arseniy Ivin <arssivka@yandex.ru>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *  @autor arssivka
+ *  @date 3/16/17
+ */
+#pragma once
+
+
+#include <queue>
+#include <functional>
+#include <condition_variable>
+#include <thread>
+#include "non_copyable.h"
+
+namespace rrc {
+    template <class T>
+    class async_worker : private non_copyable {
+    public:
+        async_worker(std::function<void(T&)> callback) : m_finished(false) {
+            m_worker = std::thread([this, callback_cap = std::move(callback)] {
+                T data;
+                while (this->wait_for_data(data)) {
+                    callback_cap(data);
+                }
+            });
+        }
+
+        void push(const T& data) {
+            {
+                std::lock_guard<std::mutex> lock(m_queue_mut);
+                m_queue.push(data);
+            }
+            m_queue_cv.notify_one();
+        }
+
+        void push(T&& data) {
+            {
+                std::lock_guard<std::mutex> lock(m_queue_mut);
+                m_queue.push(std::move(data));
+            }
+            m_queue_cv.notify_one();
+        }
+
+        ~async_worker() {
+            {
+                std::lock_guard<std::mutex> lock(m_queue_mut);
+                m_finished = true;
+            }
+            m_queue_cv.notify_one();
+            m_worker.join();
+        }
+
+    private:
+        bool wait_for_data(T& data) {
+            std::unique_lock<std::mutex> lock(m_queue_mut, std::defer_lock);
+            m_queue_cv.wait(lock, [this]{ return !m_queue.empty() || m_finished; });
+            if (m_finished) return false;
+            data = std::move(m_queue.front());
+            m_queue.pop();
+            return true;
+        }
+
+    private:
+        std::thread m_worker;
+        std::condition_variable m_queue_cv;
+        std::mutex m_queue_mut;
+        std::queue<T> m_queue;
+        bool m_finished;
+    };
+}
+
